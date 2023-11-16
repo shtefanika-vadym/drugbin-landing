8e4d2393bc9d4a52b1d9cd9e8232544267af4dbc
CostescuAdrian <adrian.costescu@assist.ro>
2023-11-16 21:42:25 +0200

    chore(drug-8): Disable web page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Routes } from 'common/routes/Routes'
+// import { Routes } from 'common/routes/Routes'
 import { Provider } from 'react-redux'
 import { store } from 'common/store/store'
 
 function App() {
   return (
     <Provider store={store}>
-      <Routes />
+      {/* <Routes /> */}
+      <></>
     </Provider>
   )
 }
